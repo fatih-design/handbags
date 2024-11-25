fa5c93e30f54eb2b7299c4b9caa0e05c7570b30a
fatih-design <tukenmezfatih@gmail.com>
2024-11-25 21:07:21 +0100

    buttons toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import './App.css'
-
+import HandbagCard from './components/handBagCard/HandbagCard';
+import Button from "./components/button/Button.jsx";
 function App() {
   return (
-      <h1>Handbags & Purses</h1>
+      <>
+          <nav>
+              <Button buttonText="to the collection" isDisabled={false} onClick={()=> console.log('to the collection')}/>
+              <Button buttonText="shop all bags" isDisabled={false} onClick={()=> console.log('shop all bags')}>shop all bags</Button>
+              <Button buttonText="pre-orders" isDisabled={true} onClick={()=> console.log('pre-orders')}>pre-orders</Button>
+          </nav>
+      <h1><HandbagCard/></h1>
+        <p><HandbagCard/></p>
+      </>
   )
 }
 
---
+++ src/components/handbagCard/HandbagCard.jsx
@@ -0,0 +1,13 @@
+import './HandbagCard.css';
+
+function HandbagCard(props) {
+
+    return (
+        <>
+            <h1>HOI</h1>
+        </>
+    );
+
+}
+
+export default HandbagCard;
